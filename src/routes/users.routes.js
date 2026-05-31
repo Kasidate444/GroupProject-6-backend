d09d7d3df8fa7c7502bffbd10f0c4a5fc8e832ab
AkkarawinS <akkarawinRX@gmail.com>
2026-05-31 17:12:29 +0700

    add wishlist and fav routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { checkUserState, login, logout, register } from '../controllers/auth.controller.js'
 import { authUser } from '../middlewares/authen.middleware.js';
-import { getUserProfile, updateUserProfile } from '../controllers/user.controller.js';
+import { getUserProfile, toggleFollowArtist, toggleWishlist, updateUserProfile } from '../controllers/user.controller.js';
 
 export const router = Router()
 
@@ -20,3 +20,10 @@ router.get('/profile',authUser, getUserProfile);
 //Update basic profile
 
 router.put('/profile' ,authUser, updateUserProfile);
+
+//favorute artist tooggle route;
+router.patch('/artists/:artistId/follow',authUser,toggleFollowArtist);
+
+//add item to wishlist tooggle route;
+router.patch('/products/:productId/wishlist',authUser,toggleWishlist);
+
